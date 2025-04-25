1de103f7f42b946047b5526ea742b0a5adb68003
Yuefii <muffidahmadiawan31@gmail.com>
2025-04-25 11:34:32 +0700

    update banner

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,17 +1,10 @@
 <template>
-  <section id="home" class="bg-[#f8b62d] py-20 md:py-32 px-4">
-    <div class="container mx-auto text-center">
-      <h1 class="text-4xl md:text-6xl font-bold mb-4">Beli tinta toner cartridge Baru dan bekas semua tipe</h1>
-      <p class="text-xl md:text-2xl mb-8 max-w-3xl mx-auto">Menyediakan tinta printer berkualitas dengan harga terbaik
-        untuk semua merek printer</p>
-      <div class="flex flex-col sm:flex-row justify-center gap-4">
-        <a href="#products"
-          class="bg-black hover:opacity-90 text-white font-bold py-3 px-6 rounded-lg transition duration-300">Lihat
-          Produk</a>
-        <a href="#contact"
-          class="bg-transparent hover:bg-black hover:text-white text-black font-bold py-3 px-6 border-2 border-black rounded-lg transition duration-300">Hubungi
-          Kami</a>
-      </div>
-    </div>
+  <section id="home" class="w-full bg-[#f8b62d]">
+      <img
+        src="/banner.png"
+        alt="Banner"
+        class="w-full h-auto object-cover  shadow-md"
+      />
+
   </section>
 </template>
